2b0c75cfd76e94530ecb6c0714689ff586568ce3
吴冬亮 <635517016@qq.com>
2019-03-22 14:04:01 +0800

    Create cornergrabber.h

---
+++ cornergrabber.h
@@ -0,0 +1,40 @@
+#ifndef CORNERGRABBER_H
+#define CORNERGRABBER_H
+
+#include <QObject>
+#include <QGraphicsItem>
+#include <QGraphicsRectItem>
+#include <QGraphicsTextItem>
+#include <QGraphicsSceneHoverEvent>
+#include <QGraphicsSceneMouseEvent>
+#include <QColor>
+#include <QPainter>
+#include <QPen>
+#include <QPointF>
+
+class CornerGrabber : public QGraphicsItem
+{
+public:
+    explicit CornerGrabber(QGraphicsItem *parent = 0);
+    qreal getHeight();
+    qreal getWidth();
+    bool isActive();
+
+    enum {bottomLeft, topLeft, topRight, bottomRight};
+
+private:
+    virtual QRectF boundingRect() const;
+    virtual void paint (QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    virtual void hoverEnterEvent ( QGraphicsSceneHoverEvent * event );
+    virtual void hoverLeaveEvent ( QGraphicsSceneHoverEvent * event );
+
+    QColor _outterborderColor;
+    QPen _outterborderPen;
+
+    qreal   _width;
+    qreal   _height;
+
+    bool _is_active;
+};
+
+#endif // CORNERGRABBER_H
