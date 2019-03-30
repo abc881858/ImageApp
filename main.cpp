4fb789a951e560e924cae0eaa65ae80d77aaab25
吴冬亮 <635517016@qq.com>
2019-03-30 17:33:15 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
+#include "mainwindow.h"
 #include <QApplication>
-#include "graphicsview.h"
-#include "net.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    qNet->initialize();
-
-    GraphicsView view;
-    view.setBackgroundBrush(QBrush(QColor(Qt::lightGray)));
-    view.showMaximized();
+    MainWindow w;
+    w.show();
 
     return a.exec();
 }
