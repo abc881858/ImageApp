d1dbb769c79c7dcf9b8067b404e5dab4d38a1e12
吴冬亮 <15721454115@189.cn>
2019-11-14 17:16:05 +0800

    Update mygraphicsrectitem.cpp

--- mygraphicsrectitem.cpp
+++ mygraphicsrectitem.cpp
@@ -1,36 +1,10 @@
 #include "mygraphicsrectitem.h"
 
-#include <QGraphicsSceneMouseEvent>
-#include <QPen>
-#include <QBrush>
-#include <QVariant>
-#include <QDebug>
-#include <QKeyEvent>
-
 MyGraphicsRectItem::MyGraphicsRectItem(const QRectF & rect, QGraphicsItem * parent)
     : QGraphicsRectItem(rect, parent)
 {
-//    setFlag(QGraphicsItem::ItemIsSelectable);
-//    setFlag(QGraphicsItem::ItemIsMovable);
     setAcceptHoverEvents(true);
-//    setFlag(QGraphicsItem::ItemIsFocusable);
-
     setPen(QPen(QBrush(QColor(Qt::green)),3));
-
-    _grabbers_created = false;
-}
-
-void MyGraphicsRectItem::deleteGrabbers()
-{
-    _bottomLeft_corner->setParentItem(NULL);
-    _topLeft_corner->setParentItem(NULL);
-    _topRight_corner->setParentItem(NULL);
-    _bottomRight_corner->setParentItem(NULL);
-
-    delete _bottomLeft_corner;
-    delete _topLeft_corner;
-    delete _topRight_corner;
-    delete _bottomRight_corner;
     _grabbers_created = false;
 }
 
@@ -56,12 +30,6 @@ void MyGraphicsRectItem::mousePressEvent(QGraphicsSceneMouseEvent *event)
     return QGraphicsItem::mousePressEvent(event);
 }
 
-//void MyGraphicsRectItem::keyPressEvent(QKeyEvent *event)
-//{
-
-//    return QGraphicsItem::keyPressEvent(event);
-//}
-
 void MyGraphicsRectItem::setCornerPositions()
 {
     QRectF rect = this->rect();
@@ -72,12 +40,6 @@ void MyGraphicsRectItem::setCornerPositions()
     _bottomRight_corner->setPos(rect.bottomRight().x()+6 - _bottomRight_corner->getWidth(), rect.bottomRight().y()+6 - _bottomRight_corner->getHeight());
 }
 
-void MyGraphicsRectItem::setLinePosition()
-{
-    line->setLine(rect().x()+rect().width()/2,rect().y(),rect().bottomLeft().x()+rect().width()/2,rect().bottomLeft().y());
-    mask->setPos(line->line().p1().x()-10,line->line().p1().y()+rect().height()/2-10);
-}
-
 void MyGraphicsRectItem::createGrabbers()
 {
     if(!grabbersAreCreated())
@@ -86,55 +48,13 @@ void MyGraphicsRectItem::createGrabbers()
         _topLeft_corner = new CornerGrabber(this);
         _topRight_corner = new CornerGrabber(this);
         _bottomRight_corner = new CornerGrabber(this);
-
-        line = new QGraphicsLineItem(this);
-        line->setLine(rect().x()+rect().width()/2,rect().y(),rect().bottomLeft().x()+rect().width()/2,rect().bottomLeft().y());
-        line->setPen(QPen(QBrush(QColor(Qt::green)),3));
-
-        mask = new QGraphicsPixmapItem(line);
-        mask->setPos(line->line().p1().x()-10,line->line().p1().y()+rect().height()/2-10);
-        mask->setPixmap(QPixmap(":/left"));
-        side = true;
-
-        line->hide();
-        visi = false;
-
         _grabbers_created = true;
         setCornerPositions();
     }
 }
 
-void MyGraphicsRectItem::changePixmap()
-{
-    if(side)
-    {
-        mask->setPixmap(QPixmap(":/right"));
-        side = false;
-    }
-    else
-    {
-        mask->setPixmap(QPixmap(":/left"));
-        side = true;
-    }
-}
-
-void MyGraphicsRectItem::changeVisiable()
-{
-    if(visi)
-    {
-        line->hide();
-        visi = false;
-    }
-    else
-    {
-        line->show();
-        visi = true;
-    }
-}
-
 int MyGraphicsRectItem::checkCornerGrabbers()
 {
-    // we return the first active found. Hopefully we never have two at the same time.
     if(_bottomLeft_corner->isActive())
         return CornerGrabber::bottomLeft;
     else if(_topLeft_corner->isActive())
