8060993aa0498349e3d51aaa2783b7dc6ef0c6d5
吴冬亮 <15721454115@189.cn>
2019-11-14 17:15:25 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include "graphicsview.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    GraphicsView view;
+    view.setBackgroundBrush(QBrush(QColor(Qt::lightGray)));
+    view.showMaximized();
 
     return a.exec();
 }
